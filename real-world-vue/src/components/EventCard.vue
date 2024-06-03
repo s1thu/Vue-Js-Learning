a17ce42e1178b7a31c3485f7cbf1b74ae08dec5f
s1thu <s1thu.w1n.5601@gmail.com>
2024-06-03 23:14:09 +0630

    Single Page Application lesson

---
+++ real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.date }}</span>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+</style>
